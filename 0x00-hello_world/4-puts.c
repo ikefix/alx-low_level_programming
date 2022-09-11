01d91be1ebaef76a4e6a0433fab5b955b9c3db91
ikefix <omekeikechukwu877@gmail.com>
2022-09-10 18:38:33 -0700

    HELLO PUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,25 @@
 #!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-	puts("\programming is like building a multilingual puzzle")
-		return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
 }
