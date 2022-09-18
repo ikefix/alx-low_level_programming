2b55e51c8b288b9fd81d333de960c28e793b8d7e
ikefix <omekeikechukwu877@gmail.com>
2022-09-17 23:56:32 -0700

    alx c programming

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - check if etter is in lowercase
+ *
+ * Return: 0 or 1
+ *
+ * @c: checking case of character
+ *
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
